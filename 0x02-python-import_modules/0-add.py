f98b51031861c8a3ab1efb7503770fa0708f4ac5
Joseph Ebin <josefophe@gmail.com>
2022-07-07 01:26:56 +0100

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 from add_0 import add
 
-a = 1
-b = 2
-c = add(a, b)
 
 if __name__ == "__main__":
+    a = 1
+    b = 2
+    c = add(a, b)
     print("{:d} + {:d} = {:d}".format(a, b, c))
